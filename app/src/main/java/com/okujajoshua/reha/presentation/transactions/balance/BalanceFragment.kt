3e40d0481e0b352e1609bd5a5cc975be4878697a
Okuja <ajokuja@nwt.ug>
2020-01-17 08:20:16 +0300

    Added view model factory

--- app/src/main/java/com/okujajoshua/reha/presentation/transactions/balance/BalanceFragment.kt
+++ app/src/main/java/com/okujajoshua/reha/presentation/transactions/balance/BalanceFragment.kt
@@ -6,6 +6,7 @@ import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
@@ -30,13 +31,15 @@ class BalanceFragment : Fragment() {
 
         //from navigation argument bundle
         val args = BalanceFragmentArgs.fromBundle(arguments!!)
-        Toast.makeText(context, "Email: ${args.email}, Password: ${args.password}", Toast.LENGTH_LONG).show()
+//        Toast.makeText(context, "Email: ${args.email}, Password: ${args.password}", Toast.LENGTH_LONG).show()
 
 
         //initialize viewmodelfactory
         viewModelFactory = BalanceViewModelFactory(args.email,args.password)
 
-        //
+        //initialize viewmodel object
+        viewModel = ViewModelProviders.of(this,viewModelFactory).get(BalanceViewModel::class.java)
+        Toast.makeText(context, "Email: ${viewModel.email}, Password: ${viewModel.password}", Toast.LENGTH_LONG).show()
 
 
         binding.addmoneyButton.setOnClickListener{view: View ->
