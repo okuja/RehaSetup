276ab03d51f1e023e6f2d4ac83573c33265b081c
Okuja <ajokuja@nwt.ug>
2020-01-28 17:05:29 +0300

    okuja | configured work manager on the android app

--- app/src/main/java/com/okujajoshua/reha/RehaApplication.kt
+++ app/src/main/java/com/okujajoshua/reha/RehaApplication.kt
@@ -1,8 +1,17 @@
 package com.okujajoshua.reha
 import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.okujajoshua.reha.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class RehaApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     /**
      * onCreate is called before the first screen is shown to the user.
@@ -12,6 +21,35 @@ class RehaApplication : Application() {
      */
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        delayedInit()
+    }
+
+    private fun setupRecurringWork(){
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    setRequiresDeviceIdle(true)
+                }
+            }
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1,TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
+    }
+
+    private fun delayedInit(){
+        applicationScope.launch{
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
     }
 }
---
+++ app/src/main/java/com/okujajoshua/reha/work/RefreshDataWorker.kt
@@ -0,0 +1,30 @@
+package com.okujajoshua.reha.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.okujajoshua.reha.database.RehaDatabase
+import com.okujajoshua.reha.repository.VideosRepository
+import retrofit2.HttpException
+import timber.log.Timber
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "com.okujajoshua.reha.work.RefreshDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val database = RehaDatabase.getInstance(applicationContext)
+        val repository = VideosRepository(database)
+        try {
+            repository.refreshVideos()
+            Timber.d("Work request for sync is run")
+
+        }catch (e: HttpException){
+            return Result.retry()
+        }
+        return Result.success()
+    }
+}
